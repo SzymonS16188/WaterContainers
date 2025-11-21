3eed82e71d9cc6380d15acc86278aa9fd8dd305d
Michal Sloniewski <michal.sloniewski@smartcodecreations.com>
2025-11-21 17:15:40 +0100

    Added serializable, equals/hahscode, getters and setters

--- src/main/java/pl/kurs/models/Car.java
+++ src/main/java/pl/kurs/models/Car.java
@@ -1,6 +1,9 @@
 package pl.kurs.models;
 
-public class Car {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Car implements Serializable {
 
     private String producer;
     private String model;
@@ -12,6 +15,42 @@ public class Car {
         this.year = year;
     }
 
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return year == car.year && Objects.equals(producer, car.producer) && Objects.equals(model, car.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(producer, model, year);
+    }
 
     @Override
     public String toString() {
