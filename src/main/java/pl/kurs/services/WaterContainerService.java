c0d0e05b28c77615fa588c35f2f3c6c942493de4
Szymon Stepniak <szymonstepniak0@gmail.com>
2025-11-23 20:49:13 +0100

    added findTheMostFilledContainerInPercentage()

--- src/main/java/pl/kurs/services/WaterContainerService.java
+++ src/main/java/pl/kurs/services/WaterContainerService.java
@@ -18,9 +18,8 @@ public class WaterContainerService {
         } while (maxWaterContainer == null && counter < array.length);
 
 
-
-      if (maxWaterContainer != null) {
-            for (int i = counter; i < array.length; i++){
+        if (maxWaterContainer != null) {
+            for (int i = counter; i < array.length; i++) {
 
                 if (array[i] != null && array[i].getWaterLevel() > maxWaterContainer.getWaterLevel()) {
                     maxWaterContainer = array[i];
@@ -28,6 +27,56 @@ public class WaterContainerService {
             }
         }
         return maxWaterContainer;
+    }
+
+    //- znalezc zbiornik ktory jest najbardziej zapelniony (procentowo) - praca domowa
+    //- znalezc wszystkie puste zbiorniki. - praca domowa
+
+    public static WaterContainer findTheMostFilledContainerInPercentage(WaterContainer[] array) {
+        if (array == null) {
+            return null;
+        }
+
+        WaterContainer mostFilledContainer = null;
+//        double mostFilledRatio = 0;
+
+        int counter = 0;
+        do {
+            mostFilledContainer = array[counter];
+            counter++;
+//            mostFilledRatio = mostFilledContainer.getWaterLevel()/mostFilledContainer.getMaxCapacity();
+        } while (mostFilledContainer == null && counter < array.length);
+
+
+//        double mostFilledRatio = mostFilledContainer.getWaterLevel() / mostFilledContainer.getMaxCapacity();
+        double mostFilledRatio = 0;
+        int counter2 = 0;
+        if (mostFilledContainer == null) {
+            return null;
+        } else {
+            mostFilledRatio = mostFilledContainer.getWaterLevel() / mostFilledContainer.getMaxCapacity();
+        }
+
+        if (mostFilledContainer != null ) {
+            for (int i = counter; i < array.length; i++) {
+                if (array[i] != null){
+                    double ratio = array[i].getWaterLevel() / array[i].getMaxCapacity();
+                    if (ratio > mostFilledRatio) {
+                        mostFilledContainer = array[i];
+                        mostFilledRatio = ratio;
+                    }
+                }
+
+//            for (WaterContainer wc : array) {
+//                double ratio = wc.getWaterLevel() / wc.getMaxCapacity();
+//                if (ratio > mostFilledRatio){
+//                    mostFilledContainer = wc;
+//                    mostFilledRatio = ratio;
+//                }
+            }
+        }
+
+        return mostFilledContainer;
     }
 
 
